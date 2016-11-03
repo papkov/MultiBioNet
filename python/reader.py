786278f767269795388ed25116074141a2aec32f
Akseshina Margarita <novmargo@gmail.com>
2016-11-03 22:09:42 +0300

    Refactoring of igraph reading

--- python/reader.py
+++ python/reader.py
@@ -11,39 +11,29 @@ def read_file(filename):
 def set_graph(df, verbal=False):
     graph = igraph.Graph()
     graph.vs["name"] = []
-    graph.es["interaction"] = []
-    graph.es["score"] = []
 
     for index, row in df.iterrows():
-        ensg1 = row['ensg1']
-        ensg2 = row['ensg2']
-        score = row['score']
-        interaction = row['interaction']
 
-        if ensg1 not in graph.vs["name"]:
-            graph.add_vertex(name=ensg1)
-            ensg1_index = len(graph.vs["name"]) - 1
-        else:
-            ensg1_index = graph.vs["name"].index(ensg1)
+        new_edge = []
 
-        if ensg2 not in graph.vs["name"]:
-            graph.add_vertex(name=ensg2)
-            ensg2_index = len(graph.vs["name"]) - 1
-        else:
-            ensg2_index = graph.vs["name"].index(ensg2)
+        for ensg in [row['ensg1'], row['ensg2']]:
+            if ensg not in graph.vs["name"]:
+                graph.add_vertex(name=ensg)
+                new_edge.append(len(graph.vs["name"]) - 1)
+            else:
+                new_edge.append(graph.vs["name"].index(ensg))
 
+        graph.add_edge(new_edge[0], new_edge[1],
+                       interaction=row['interaction'], score=row['score'])
         if verbal:
-            print("Add edge between %s, %s" % (ensg1_index, ensg2_index))
-
-        graph.add_edges([(ensg1_index, ensg2_index)])
-        graph.es["interaction"] = interaction
-        graph.es["score"] = score
+            print("Add edge between %s, %s" % (new_edge[0], new_edge[1]))
 
     igraph.summary(graph)
     return graph
 
 
-df = read_file("../data/alz_intact_int_PPI.txt")
-g = set_graph(df)
-print(g.vs["name"])
-igraph.plot(g)
+if __name__ == '__main__':
+    df = read_file("../data/alz_intact_int_PPI.txt")
+    g = set_graph(df)
+    #print(g.vs["name"])
+    igraph.plot(g)
