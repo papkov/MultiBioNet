f476e7a4742dbe89cff28245f6953fe636d993b4
Mikhail Papkov <mikhail.papkov@gmail.com>
2016-12-10 11:26:45 +0300

    ENSG for DISEASES

---
+++ R/ENSG_to_ENSP.R
@@ -0,0 +1,13 @@
+library(biomaRt)
+# define biomart object
+mart <- useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
+
+# Test
+# query biomart
+results <- getBM(attributes = c("ensembl_gene_id", "ensembl_transcript_id", "ensembl_peptide_id"),
+                 filters = "ensembl_peptide_id", values = "ENSP00000053867",
+                 mart = mart)
+results
+
+
+diseases <- read.csv('D:\\Projects\\MultiBioNet\\data\\alz_DISEASES', sep = '\t', header = T)
